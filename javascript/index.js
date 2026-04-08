dcd4c8ca4b419db56dd7dd71b48dc9a7a7e3913e
mkvolk <markoscalderonqwerty@gmail.com>
2026-04-08 14:40:58 -0400

    Implemented addItem

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,17 @@
+var receiptCounter = 0;
+var itemCounter = 0;
 
+const clear = () => {
+
+};
+
+const fillRandom = () => {
+
+    console.log("form filled with random data");
+};
 
 const downloadHandler = (fileName,content,extension) => {
-    const text =content ;
+    const text = content ;
     const element = document.createElement('a');
     const mimeType = 'application/octect-stream';
     var type = extension;
@@ -13,10 +23,38 @@ const downloadHandler = (fileName,content,extension) => {
     document.body.appendChild(element);
     element.click();
     document.body.removeChild(element);
+
+    console.log("Receipt Download"); //DEBUG
   };
 
   const addItem = () => {
     const formItemsElement = document.querySelector('formItems');
+    const currentIndex = Math.floor(itemCounter).toString;
 
+    var item = document.createElement('item');
+    var itName = document.createElement('input');
+    var itPrice = document.createElement('input');
+    var itQty = document.createElement('input');
+    var itExtPrice = document.createElement('input');
+
+    itName.id = 'it_name-' + currentIndex;
+    itName.type = "text";
+
+    itPrice.id = 'it_price-' + currentIndex;
+    itPrice.type = 'number';
+
+    itQty.id = 'it_qty-' + currentIndex;
+    itQty.type = 'number';
+
+    itExtPrice.id = 'it_ext-price-' + currentIndex;
+    itExtPrice.type = 'number';
+
+    item.id = 'item-' + currentIndex;
+    item.appendChild(itName);
+    item.appendChild(itPrice);
+    item.appendChild(itQty);
+    item.appendChild(itExtPrice);
+
+    formItemsElement.appendChild(item);
 
   }
