e70f279c59226fcf98114c6a2a8010655f7e39a2
mkvolk <markoscalderonqwerty@gmail.com>
2026-04-08 11:42:46 -0400

    First Commit

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+
+
+const downloadHandler = (fileName,content) => {
+    const text =content ;
+    const element = document.createElement('a');
+    const file = new Blob([text], {type: 'text/plain'});
+    element.href = URL.createObjectURL(file);
+    element.download = fileName;
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+  };
