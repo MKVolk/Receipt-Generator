746a261c8e901b26dfe57d741b26bcfce8b4c83f
Aayatullah Adedoja Adeyemi <134182609+Adedoja@users.noreply.github.com>
2026-04-12 17:11:53 -0400

    Add login and signup functions in script.js

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+function handleLogin(){
+    const role = document.querySelector('input[name="role"]:checked').value;
+    const phone = document.getElementByID('phone').value;
+    const password = document.getElementByID('password').value;
+
+    if (phone === 12345 && password ==="pawonboy"){
+        localStorage.setItem("UserRole", role);
+        localStorage.setItem("userphone", phone);
+    
+    if (role === "merchant"){
+        window.location.href = "merchant_generator.uml";
+
+    }
+    else{
+        window.location.href ="customer_dashboard.html";
+    }
+}
+else {
+    alert("Incorrect phone number or password");
+}
+}
+function handleSignup(){
+    alert("we will add a signup page as we update the app")
+}
